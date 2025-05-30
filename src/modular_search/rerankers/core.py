6ae78e37b13efd9273ca0ef9cbafe5b626e80868
Prannaya Gupta <prannayagupta@gmail.com>
2025-05-30 19:55:01 +0800

    chore(reranker): modify core reranker for better functional utility

--- src/modular_search/rerankers/core.py
+++ src/modular_search/rerankers/core.py
@@ -1,20 +1,18 @@
-from abc import ABC, abstractmethod
-from typing import Generic, TypeVar, List, Dict, Any
+from typing import Generic, TypeVar, List
 
 
 C = TypeVar('C')
 
-class Reranker(ABC, Generic[C]):
-    @abstractmethod
+class Reranker(Generic[C]):
     def rerank(self, query: str, candidates: List[C]) -> List[C]:
         """
         Reranks the given candidates based on the query.
         
         Args:
             query (str): The search query.
-            candidates (List[Dict[str, Any]]): The list of candidate documents to rerank.
+            candidates (List[C]): The list of candidate documents to rerank.
         
         Returns:
-            List[Dict[str, Any]]: The reranked list of candidates.
+            List[C]: The reranked list of candidates.
         """
-        pass
+        return candidates
